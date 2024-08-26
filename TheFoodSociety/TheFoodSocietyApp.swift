4474b876dcec6526801e40d8c53feaa90bd9ebcd
Celeste Roselli <72556836+celesteroselli@users.noreply.github.com>
2024-08-26 18:26:03 -0400

    Initial Commit

---
+++ TheFoodSociety/TheFoodSocietyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheFoodSocietyApp.swift
+//  TheFoodSociety
+//
+//  Created by Celeste Roselli on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct TheFoodSocietyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
