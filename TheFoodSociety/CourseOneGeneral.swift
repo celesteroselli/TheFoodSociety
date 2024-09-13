436b81133c71a6dfe35d9893c135ea0adea9cad2
Celeste Roselli <72556836+celesteroselli@users.noreply.github.com>
2024-09-12 21:19:32 -0400

    Added UI files to work on

---
+++ TheFoodSociety/CourseOneGeneral.swift
@@ -0,0 +1,18 @@
+//
+//  CourseOneGeneral.swift
+//  TheFoodSociety
+//
+//  Created by Celeste Roselli on 9/12/24.
+//
+
+import SwiftUI
+
+struct CourseOneGeneral: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CourseOneGeneral()
+}
---
+++ TheFoodSociety/Dashboard.swift
@@ -0,0 +1,18 @@
+//
+//  Dashboard.swift
+//  TheFoodSociety
+//
+//  Created by Celeste Roselli on 9/12/24.
+//
+
+import SwiftUI
+
+struct Dashboard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Dashboard()
+}
